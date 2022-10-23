ee02ec6bcf9c342c44c20424cf090431db8a9bdf
Arkadiusz M <arek.madejek@gmail.com>
2022-10-23 20:09:06 +0200

    add three test to registerpage scenario

--- cypress/integration/PageObjects/RegisterPage.js
+++ cypress/integration/PageObjects/RegisterPage.js
@@ -3,6 +3,7 @@ class RegisterPage{
         SignUpBtn:() => cy.get('.login').contains('Sign in'),
         RegisterTitle:() => cy.get('#create-account_form h3'),
         EmailInput:() => cy.get('#email_create'),
+        InvalidEmailText:() => cy.get('#create_account_error'),
         CreateAccountBtn:() => cy.get('#SubmitCreate').contains('Create an account'),
         HeadingRegisterTitle:() => cy.get('#account-creation_form h3').eq(0),
         CustomerName:() => cy.get('#customer_firstname'),
@@ -18,20 +19,23 @@ class RegisterPage{
         SelectYear:() => cy.get('#years'),
         AssertDateTitle:() => cy.get('label').contains('Date of Birth'),
         AddressInput:() => cy.get('#address1'),
-        AssertAdressTitle:() => cy.get('label[for="address1"]')
-
+        AssertAdressTitle:() => cy.get('label[for="address1"]'),
+        NewsletterCheck:() => cy.get('#newsletter'),
+        OffersCheck:() => cy.get('#optin'),
+        CityInput:() => cy.get('#city'),
+        AssertCityTitle:() => cy.get('label[for="city"]'),
+        StateSelect:() => cy.get('#id_state'),
+        AssertStateTitle:() => cy.get('label[for="id_state"]'),
+        PostcodeInput:() => cy.get('#postcode'),
+        AssertPostcodeTitle:() => cy.get('label[for="postcode"]'),
+        PhoneNumberInput:() => cy.get('#phone_mobile'),
+        AssertPhoneTitle:() => cy.get('label[for="phone_mobile"]'),
+        SubmitBtn:() => cy.get('#submitAccount'),
+        AssertRegistration:() => cy.get('.info-account'),
+        LogoutBtn:() => cy.get('.logout'),
+        AlertIncorectData:() => cy.get('.alert-danger')
     }
-    ChooseSex(sex){
-        if (sex === 'male'){
-            this.elements.MaleCheck().click()
-                                     .should('be.checked');
-        }
-        else if(sex === 'female'){
-            this.elements.FemaleCheck().click()
-                                       .should('be.checked');
-        }
-        return this;
-    }
+    
     GoToRegisterPage(text){
         this.elements.SignUpBtn().click();
         this.elements.RegisterTitle().should('contain', text);
@@ -42,6 +46,23 @@ class RegisterPage{
         this.elements.CreateAccountBtn().click();
         this.elements.HeadingRegisterTitle().should('contain',headingTitle);
         return this;
+    }
+    InvalidInputWrongEmailFormat(email, text){
+        this.elements.EmailInput().type(email);
+        this.elements.CreateAccountBtn().click();
+        this.elements.InvalidEmailText().should('contain', text);
+        return this;
+    }
+    ChooseSex(sex){
+            if (sex === 'male'){
+                this.elements.MaleCheck().click()
+                                         .should('be.checked');
+            }
+            else if(sex === 'female'){
+                this.elements.FemaleCheck().click()
+                                           .should('be.checked');
+            }
+            return this;
     }
     InputCustomerName(title, name){
         this.elements.AssertTitleName().should('contain', title);
@@ -65,9 +86,61 @@ class RegisterPage{
         this.elements.SelectYear().select(year);
         return this;
     }
-    InputAddress(title,address){
+    CheckAgreement(newsletterAgreement, offersAgreement){
+        if (newsletterAgreement === 'yes'){
+            this.elements.NewsletterCheck().check().should('be.checked');
+        }
+        else {
+            this.elements.NewsletterCheck().should('not.be.checked')
+        }
+        if (offersAgreement === 'yes'){
+            this.elements.OffersCheck().check().should('be.checked');
+        }
+        else {
+            this.elements.OffersCheck().should('not.be.checked')
+        }
+        return this;
+    }
+    InputAddress(title, address){
         this.elements.AssertAdressTitle().should('contain',title);
         this.elements.AddressInput().type(address);
+        return this;
+    }
+    InputCity(title, city){
+        this.elements.AssertCityTitle().should('contain',title);
+        this.elements.CityInput().type(city);
+        return this;
+    }
+    SelectState(title, state){
+        this.elements.AssertStateTitle().should('contain',title);
+        this.elements.StateSelect().select(state)
+        return this;
+    }
+    InputPostcode(title, postcode){
+        this.elements.AssertPostcodeTitle().should('contain',title);
+        this.elements.PostcodeInput().type(postcode);
+        return this;
+    }
+    InputPhoneNumber(title, phoneNo){
+        this.elements.AssertPhoneTitle().should('contain',title);
+        this.elements.PhoneNumberInput().type(phoneNo);
+        return this;
+    }
+    SubmitRegistration(){
+        this.elements.SubmitBtn().click();
+        return this;
+    }
+    LogInPageAssertion(title){
+        this.elements.AssertRegistration().should('contain', title);
+        return this;
+    }
+    Logout(title){
+        this.elements.LogoutBtn().click();
+        this.elements.SignUpBtn().should('be.visible');
+        return this;
+    }
+    LogWithIncorectDataAlertAssertion(text){
+        this.elements.AlertIncorectData().should('contain', text);
         return this;
     }
        
--- cypress/integration/Tests/RegistrationTest.js
+++ cypress/integration/Tests/RegistrationTest.js
@@ -8,9 +8,24 @@ describe ('Register new user and checking communicates with wrong data', () => {
     })
     it('Valid registration test', () => {
         const page = new RegisterPage();
-        let number = Math.random() * 10000;
-        let day = Math.round(Math.random() * 31).toString();
+        let number = Math.round(Math.random() * 10000);
         let month = Math.round(Math.random() * 12).toString();
+        if (month === '0'){
+            month = '1';
+        }
+        let day;
+        if (month === '1' || '3' || '5' || '7' || '8' || '10' || '12'){
+            day = Math.round(Math.random() * 31).toString();
+        } 
+        else if (month === '4' || '6' || '9' || '11'){
+            day = Math.round(Math.random() * 30).toString();
+        } 
+        else {
+            day = Math.round(Math.random() * 29).toString();
+        }
+        if (day === '0'){
+            day = '1';
+        }
         let year = Math.round(Math.random() * (2022 - 1900 +1) + 1900).toString();
         page.GoToRegisterPage('Create an account')
             .InputEmail('user' + number + '@example.com', 'Your personal information')
@@ -19,7 +34,44 @@ describe ('Register new user and checking communicates with wrong data', () => {
             .InputLastName('Last name', 'Blue')
             .InputPassword('Password', 'Tester123')
             .SelectDayOfBirth('Date of Birth', day, month, year)
-            .InputAddress('Address', 'Long Street 32');
+            .CheckAgreement('yes','no')
+            .InputAddress('Address', 'Long Street 32')
+            .InputCity('City', 'New York')
+            .SelectState('State', 'New York')
+            .InputPostcode('Zip/Postal Code', '12432')
+            .InputPhoneNumber('Mobile phone','432754213')
+            .SubmitRegistration()
+            .LogInPageAssertion('Welcome to your account.');
 
+    })
+    it('Logout user test', () => {
+        const page = new RegisterPage();
+        let number = Math.round(Math.random() * 10000);
+        page.GoToRegisterPage('Create an account')
+        .InputEmail('user' + number + '@example.com', 'Your personal information')
+        .InputCustomerName('First name', 'John')
+        .InputLastName('Last name', 'Blue')
+        .InputPassword('Password', 'Tester123')
+        .InputAddress('Address', 'Yelowside 542')
+        .InputCity('City', 'New York')
+        .SelectState('State', 'New York')
+        .InputPostcode('Zip/Postal Code', '12432')
+        .InputPhoneNumber('Mobile phone','432754213')
+        .SubmitRegistration()
+        .LogInPageAssertion('Welcome to your account.')
+        .Logout();
+    })
+    it('Invalid e-mail test', () => {
+        const page = new RegisterPage();
+        page.GoToRegisterPage('Create an account')
+            .InvalidInputWrongEmailFormat('wrong', 'Invalid email address.');
+    })
+    it('Submit without write form attributes', () => {
+        const page = new RegisterPage();
+        let number = Math.round(Math.random() * 10000);
+        page.GoToRegisterPage('Create an account')
+            .InputEmail('user' + number + '@example.com', 'Your personal information')
+            .SubmitRegistration()
+            .LogWithIncorectDataAlertAssertion('There are 8 errors');
     })
 })
